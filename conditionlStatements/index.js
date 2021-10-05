20eebaedeb4c727e90effeb536a655be4d7dc283
Dan Ngetich <dancanmilgo73@gmail.com>
2021-10-05 17:06:12 +0300

    final tweak

--- conditionlStatements/index.js
+++ conditionlStatements/index.js
@@ -35,4 +35,5 @@ const getInputAndDisplay = () => {
     process.exit();
   });
 };
+
 getInputAndDisplay();
--- fizzBuzz/index.js
+++ fizzBuzz/index.js
@@ -10,4 +10,4 @@ const fizzBuzz = () => {
   }
 };
 
-console.log(fizzBuzz());
+fizzBuzz();
